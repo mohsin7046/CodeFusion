cc09af30d3782d0f52628b1cb51221a2cdde14db
Mohsinali Datardi <datardimohsinali@gmail.com>
2024-12-23 23:47:52 +0530

    Landing page component are created

--- Interview_Platform/src/App.tsx
+++ Interview_Platform/src/App.tsx
@@ -1,11 +1,11 @@
 // import { useState } from 'react';
 import './App.css';
-
+import Landing_Page from './common_Page/Landing_Page';
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <div>Hello</div>
+    <Landing_Page />
   );
 }
 
---
+++ Interview_Platform/src/common_Page/Landing_Page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Landing_Page() {
+  return (
+  <>
+  
+  </>
+    
+  )
+}
+
+export default Landing_Page
